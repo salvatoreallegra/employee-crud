62610ece167c55d1cb4c4b56adcc7e38dfc9c2f2
Salvatore Allegra <salallegra@gmail.com>
2020-09-25 05:32:53 -0400

    Create post request method

--- employeecrud/src/main/java/com/salallegra/employeecrud/controller/EmployeeController.java
+++ employeecrud/src/main/java/com/salallegra/employeecrud/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,12 @@ public class EmployeeController {
 	public List<Employee> getAllEmployees(){
 		
 		return repository.findAll();
+	}
+	
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		
+		return repository.save(employee);
 	}
 
 }

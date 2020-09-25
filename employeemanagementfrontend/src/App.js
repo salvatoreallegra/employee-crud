504e59d2450fcc333637d4678c77e9d5e3e1cb73
Salvatore Allegra <salallegra@gmail.com>
2020-09-25 04:45:07 -0400

    Create header and footer components

--- employeemanagementfrontend/src/App.js
+++ employeemanagementfrontend/src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ListEmployeeComponent from './components/ListEmployeeComponent';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
-    
-    <div className="container">
-     
-     <ListEmployeeComponent></ListEmployeeComponent>
+    <div >
+      <HeaderComponent />
+      <div className="container">
+        <ListEmployeeComponent />
+      </div>
+      <FooterComponent />
     </div>
-    
   );
 }
 

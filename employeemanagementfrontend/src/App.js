9d7bc5bba9770689bbe681fc8db3d88f0b6290f4
Salvatore Allegra <salallegra@gmail.com>
2020-09-28 22:37:44 -0400

    Complete create employee form

--- employeemanagementfrontend/src/App.js
+++ employeemanagementfrontend/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
 
 function App() {
   return (
@@ -13,8 +13,12 @@ function App() {
         <HeaderComponent />
         <div className="container">
           <Switch>
-            <Route path="/" component={ListEmployeeComponent}></Route>
+            <Route path="/" exact component={ListEmployeeComponent}></Route>
             <Route path="/employees" component={ListEmployeeComponent}></Route>
+            <Route
+              path="/create-employees"
+              component={CreateEmployeeComponent}
+            ></Route>
           </Switch>
         </div>
         <FooterComponent />
--- employeemanagementfrontend/src/components/ListEmployeeComponent.js
+++ employeemanagementfrontend/src/components/ListEmployeeComponent.js
@@ -1,53 +1,57 @@
-import React, { Component } from 'react'
-import EmployeeService from '../Services/EmployeeService';
+import React, { Component } from "react";
+import EmployeeService from "../Services/EmployeeService";
 
 export default class ListEmployeeComponent extends Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            employees: []
-        }
-    }
+    this.state = {
+      employees: [],
+    };
+    this.addEmployee = this.addEmployee.bind(this);
+  }
 
-    componentDidMount(){
-        EmployeeService.getEmployees().then((res) => {
-            this.setState({employees: res.data})
-        });
-    }
+  componentDidMount() {
+    EmployeeService.getEmployees().then((res) => {
+      this.setState({ employees: res.data });
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <h2 className="text-center">Employee List</h2>
-                <div className = "row">
-                    <table className="table table-striped table-bordered">
+  addEmployee() {
+    this.props.history.push("/create-employees");
+  }
 
-                        <thead>
-                            <tr>
-                                <th>Employee First Name</th>
-                                <th>Employee Last Name</th>
-                                <th>Employee Email Id</th>
-                                <th>Employee Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                            {this.state.employees.map(employee =>
-                                <tr key = {employee.id}>
-                                    <td>{employee.firstName}</td>
-                                    <td>{employee.lastName}</td>
-                                    <td>{employee.emailId}</td>
-                                </tr>
-                            )
-                            }
-                        </tbody>
-                    </table>
-
-
-
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h2 className="text-center">Employee List</h2>
+        <div className="row ">
+          <button className="btn btn-primary" onClick={this.addEmployee}>
+            Add Employee
+          </button>
+        </div>
+        <div className="row">
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Employee First Name</th>
+                <th>Employee Last Name</th>
+                <th>Employee Email Id</th>
+                <th>Employee Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.employees.map((employee) => (
+                <tr key={employee.id}>
+                  <td>{employee.firstName}</td>
+                  <td>{employee.lastName}</td>
+                  <td>{employee.emailId}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
